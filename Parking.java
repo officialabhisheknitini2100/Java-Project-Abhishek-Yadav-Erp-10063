aa5242190df4c8643ad1cd763f84d410c79372a7
abhishek_nitin21 <officialabhisheknitini210@gmail.com>
2026-06-05 10:05:11 +0530

    Refactor Parking System to manage multiple cars
    Added abstraction using Vehicle class.
    Implemented multiple parking slots.
    Added functionality to park, remove, and display cars.
    Improved overall parking management system.

--- Parking.java
+++ Parking.java
@@ -1,53 +1,109 @@
 import java.util.Scanner;
 
-class Car {
+abstract class Vehicle {
     int id;
 
+    Vehicle(int id) {
+        this.id = id;
+    }
+
+    abstract void display();
+}
+
+class Car extends Vehicle {
 
     Car(int id) {
-        this.id = id;
+        super(id);
+    }
+
+    void display() {
+        System.out.println("Car ID: " + id);
     }
 }
 
-public class Parking {
+public class ParkingSystem {
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        Car c = null;
+
+        Car[] parking = new Car[5]; // 5 parking slots
 
         while (true) {
+
             System.out.println("\n1. Park Car");
             System.out.println("2. Remove Car");
-            System.out.println("3. Exit");
+            System.out.println("3. Show Cars");
+            System.out.println("4. Exit");
 
             int choice = sc.nextInt();
 
-            if (choice == 1) {
-                if (c == null) {
+            switch (choice) {
+
+                case 1:
                     System.out.print("Enter Car ID: ");
                     int id = sc.nextInt();
-                    c = new Car(id);
-                    System.out.println("Car parked");
-                } else {
-                    System.out.println("Parking already occupied!");
-                }
-            }
 
-            else if (choice == 2) {
-                if (c != null) {
-                    System.out.println("Car " + c.id + " removed");
-                    c = null;
-                } else {
-                    System.out.println("No car to remove!");
-                }
-            }
+                    boolean parked = false;
 
-            else if (choice == 3) {
-                break;
-            }
+                    for (int i = 0; i < parking.length; i++) {
+                        if (parking[i] == null) {
+                            parking[i] = new Car(id);
+                            System.out.println("Car parked at slot " + (i + 1));
+                            parked = true;
+                            break;
+                        }
+                    }
 
-            else {
-                System.out.println("Invalid choice!");
+                    if (!parked) {
+                        System.out.println("Parking Full!");
+                    }
+                    break;
+
+                case 2:
+                    System.out.print("Enter Car ID to remove: ");
+                    int removeId = sc.nextInt();
+
+                    boolean found = false;
+
+                    for (int i = 0; i < parking.length; i++) {
+                        if (parking[i] != null && parking[i].id == removeId) {
+                            parking[i] = null;
+                            found = true;
+                            System.out.println("Car Removed");
+                            break;
+                        }
+                    }
+
+                    if (!found) {
+                        System.out.println("Car Not Found");
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("\nParked Cars:");
+
+                    boolean empty = true;
+
+                    for (int i = 0; i < parking.length; i++) {
+                        if (parking[i] != null) {
+                            System.out.print("Slot " + (i + 1) + " -> ");
+                            parking[i].display();
+                            empty = false;
+                        }
+                    }
+
+                    if (empty) {
+                        System.out.println("Parking Empty");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Exiting...");
+                    return;
+
+                default:
+                    System.out.println("Invalid Choice");
             }
         }
     }
